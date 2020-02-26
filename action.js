c390df10b7df0d79b8c3ea2db0ce2cb10e867a65
Ariana Freitag <freitagariana@gmail.com>
2020-02-26 15:46:54 -0500

    little fix

--- action.js
+++ action.js
@@ -60,27 +60,25 @@ class Ball {
   }
 
   collide() {
-    if (!locked[this.id]) {
-      for (let i = this.id + 1; i < numBalls; i++) {
-        // console.log(others[i]);
-        let dx = this.others[i].x - this.x;
-        let dy = this.others[i].y - this.y;
-        let distance = sqrt(dx * dx + dy * dy);
-        let minDist = this.others[i].diameter / 2 + this.diameter / 2;
-        //   console.log(distance);
-        //console.log(minDist);
-        if (distance < minDist) {
-          //console.log("2");
-          let angle = atan2(dy, dx);
-          let targetX = this.x + cos(angle) * minDist;
-          let targetY = this.y + sin(angle) * minDist;
-          let ax = (targetX - this.others[i].x) * spring;
-          let ay = (targetY - this.others[i].y) * spring;
-          this.vx -= ax;
-          this.vy -= ay;
-          this.others[i].vx += ax;
-          this.others[i].vy += ay;
-        }
+    for (let i = this.id + 1; i < numBalls; i++) {
+      // console.log(others[i]);
+      let dx = this.others[i].x - this.x;
+      let dy = this.others[i].y - this.y;
+      let distance = sqrt(dx * dx + dy * dy);
+      let minDist = this.others[i].diameter / 2 + this.diameter / 2;
+      //   console.log(distance);
+      //console.log(minDist);
+      if (distance < minDist) {
+        //console.log("2");
+        let angle = atan2(dy, dx);
+        let targetX = this.x + cos(angle) * minDist;
+        let targetY = this.y + sin(angle) * minDist;
+        let ax = (targetX - this.others[i].x) * spring;
+        let ay = (targetY - this.others[i].y) * spring;
+        this.vx -= ax;
+        this.vy -= ay;
+        this.others[i].vx += ax;
+        this.others[i].vy += ay;
       }
     }
   }
